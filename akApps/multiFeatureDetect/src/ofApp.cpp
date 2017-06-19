1c3c3cf24c54e9290c941d45facea7f3d5ed5091
A. K. Williams <akwilliams@akwilliams.local>
2017-06-18 21:34:16 -0400

    multi-feature detect working

--- akApps/multiFeatureDetect/src/ofApp.cpp
+++ akApps/multiFeatureDetect/src/ofApp.cpp
@@ -21,15 +21,16 @@ void ofApp::setup(){
     showFaceProfile = false;
     
     eye.setup("haarcascade_eye.xml");
-    // eye.setPreset(ObjectFinder::Fast);
+     eye.setPreset(ObjectFinder::Fast);
     face.setup("haarcascade_frontalface_default.xml");
-    // face.setPreset(ObjectFinder::Fast);
+     face.setPreset(ObjectFinder::Fast);
     face_alt.setup("haarcascade_frontalface_alt.xml");
-    // face_alt.setPreset(ObjectFinder::Fast);
+     face_alt.setPreset(ObjectFinder::Fast);
     face_profile.setup("haarcascade_profileface.xml");
-    // face_profile.setPreset(ObjectFinder::Fast);
+     face_profile.setPreset(ObjectFinder::Fast);
     
     cam.setup(640,480);
+    ofEnableAlphaBlending();
 }
 
 
@@ -56,33 +57,38 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    cam.draw(0,0);
     if(showEye){
         eye.draw();
+        ofDrawBitmapStringHighlight("drawing eye", 0, 15);
     }
     if(showFace){
         face.draw();
+        ofDrawBitmapStringHighlight("drawing face", 0, 45);
     }
     if(showFaceAlt){
         face_alt.draw();
+        ofDrawBitmapStringHighlight("drawing face alt", 0, 75);
     }
     if(showFaceProfile){
         face_profile.draw();
+        ofDrawBitmapStringHighlight("drawing face profile", 0, 105);
     }
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
     switch(key){
-        case 1:
+        case '1':
             showEye = !showEye;
             break;
-        case 2:
+        case '2':
             showFace = !showFace;
             break;
-        case 3:
+        case '3':
             showFaceAlt = !showFaceAlt;
             break;
-        case 4:
+        case '4':
             showFaceProfile = !showFaceProfile;
             break;
             
